712d1feb94f6d779eb4494b27de07e81d2221f65
Arsenii Margasov <margasovarsenij@gmail.com>
2021-03-20 12:54:09 +0300

    small changes for pylint

--- hw3/reducer_top10_by_year.py
+++ hw3/reducer_top10_by_year.py
@@ -7,10 +7,10 @@ year_2010, year_2016 = 0, 0
 
 for line in sys.stdin:
     year, tag_counts = line.strip().split("\t", 1)
-    if "2010" == year and year_2010 < AMOUNT_OF_STRINGS:
+    if year == "2010" and year_2010 < AMOUNT_OF_STRINGS:
         print(year, tag_counts, sep="\t")
         year_2010 += 1
-    elif "2016" == year and year_2016 < AMOUNT_OF_STRINGS:
+    elif year == "2016" and year_2016 < AMOUNT_OF_STRINGS:
         print(year, tag_counts, sep="\t")
         year_2016 += 1
     else:
